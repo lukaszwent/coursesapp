cf5cdf048280634a4621453d28ea0dd25a17b864
zakumb <zakzyne@gmail.com>
2022-01-23 02:16:39 +0100

    Authentication added

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const methodOverride = require("method-override");
 require("dotenv").config();
@@ -6,108 +5,233 @@ const path = require("path");
 const PORT = process.env.PORT || 3000;
 const DB_HOST = process.env.DB_HOST;
 const app = express();
-const {connect} = require("./db");
-const coursesSchema = require('./models/courses');
+const { connect } = require("./db");
+const coursesSchema = require("./models/courses");
 const ejsmate = require("ejs-mate");
-
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const User = require("./models/user");
 connect(DB_HOST);
 
-app.engine("ejs",ejsmate);
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
+app.engine("ejs", ejsmate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname, 'public')))
-/*
-Podajesz : Nazwa, Czas Trwania,img, ile obejrzałeś już minut : default:0 --procent ukończenia liczy ci apka 
-    GET /                           -Homepage
-    GET /courses                    -Wszystkie kursy użytkownika(które po kliknięciu rozciagają szczególy) po 5 na strone
-    POST /courses                   -Dodaj nowy kurs
-    -------------------GET /courses/new                -Nowy formularz dodawaniu kursów
-    GET /courses/:id                -Wyświetla szczegóły danego kursu
-    PATCH /courses/:id              -Umożliwia zmiane konkretnej wartości reszta , wybiera ją klikając na szare pole i wpisując nową wartość
-    DELETE /courses/:id             -Usuwa dany kurs
-    PATCH /courses/:id/addMinutes   -Umożliwia dodanie ilości objerzanych minut w kursie
-*/
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session({ secret: "secretpass" }));
+app.use((req, res, next) => {
+  res.locals.user_id = req.session.user_id;
+  next();
+});
+const isLoggedIn = (req, res, next) => {
+  if (!req.session.user_id) {
+    req.session.lastUrl = req.originalUrl;
+    return res.redirect("/login");
+  }
+  next();
+};
 
-app.get("/",(req,res)=>{
-    res.render("home");
-})
-app.get("/courses",async(req,res)=>{
-    const allcourses = await coursesSchema.find({isArchived:false});
-    //console.log(allcourses);
-    res.render("courses/index",{allcourses});
-})
-app.post("/courses",async (req,res)=>{
+const isAuthor = async (req, res, next) => {
+  const id = req.params.id;
+  const course = await coursesSchema.findById(id);
 
-    const btime = req.body.timeFormat === "hours" ? req.body.time *60 : req.body.time;
-    if(!req.body.image)
-        delete req.body.image;
-    let reqBody = {...req.body,time: btime};
-    const course = new coursesSchema({...reqBody});
-    course.save();
+  if (!course.author.equals(req.session.user_id)) {
+    return res.redirect("/courses");
+  }
+
+  next();
+};
+
+const catchErrorAsync = (surroundedFunc) => (req, res, next) => {
+  surroundedFunc(req, res, next).catch((e) => next(e));
+};
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post(
+  "/register",
+  catchErrorAsync(async (req, res) => {
+    const { password, username } = req.body;
+
+    const userExist = await User.findOne({ username });
+    console.log(userExist);
+    if (userExist) return res.redirect("/register");
+
+    cryptedPassword = await bcrypt.hash(password, 12);
+    const user = new User({ username, password: cryptedPassword });
+    await user.save();
+    req.session.user_id = user._id;
     res.redirect("/courses");
-})
+  })
+);
 
-app.get("/courses/new",(req,res)=>{
-    res.render("courses/newForm");
-})
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
-app.get("/courses/:id",async (req,res)=>{
+app.post(
+  "/login",
+  catchErrorAsync(async (req, res) => {
+    const { username, password } = req.body;
+    const foundUser = await User.findOne({ username });
+
+    if (!foundUser) return res.redirect("/login");
+
+    const isValid = bcrypt.compare(password, foundUser.password);
+    if (isValid) {
+      req.session.user_id = foundUser._id;
+      if (req.session.lastUrl) res.redirect(req.session.lastUrl);
+      else res.redirect("/courses");
+    } else {
+      res.redirect("/login");
+    }
+  })
+);
+
+app.post("/logout", (req, res) => {
+  req.session.user_id = null;
+  req.session.lastUrl = null;
+  res.redirect("/login");
+});
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get(
+  "/courses",
+  isLoggedIn,
+  catchErrorAsync(async (req, res) => {
+    const allcourses = await coursesSchema.find({
+      isArchived: false,
+      author: req.session.user_id,
+    });
+    res.render("courses/index", { allcourses });
+  })
+);
+
+app.post(
+  "/courses",
+  isLoggedIn,
+  catchErrorAsync(async (req, res) => {
+    const btime =
+      req.body.timeFormat === "hours" ? req.body.time * 60 : req.body.time;
+    if (!req.body.image) delete req.body.image;
+    let reqBody = { ...req.body, time: btime, author: req.session.user_id };
+    const course = new coursesSchema({ ...reqBody });
+    await course.save();
+    res.redirect("/courses");
+  })
+);
+
+app.get("/courses/new", isLoggedIn, (req, res) => {
+  res.render("courses/newForm");
+});
+
+app.get(
+  "/courses/:id",
+  isLoggedIn,
+  isAuthor,
+  catchErrorAsync(async (req, res) => {
     const course = await coursesSchema.findById(req.params.id);
-    res.render("courses/show",{course});
-})
+    res.render("courses/show", { course });
+  })
+);
 
-app.get("/courses/:id/edit",async (req,res)=>{
+app.get(
+  "/courses/:id/edit",
+  isLoggedIn,
+  isAuthor,
+  catchErrorAsync(async (req, res) => {
     const course = await coursesSchema.findById(req.params.id);
-    res.render("courses/editForm",{course});
-})
+    res.render("courses/editForm", { course });
+  })
+);
 
-app.put("/courses/:id",async (req,res)=>{
-    //console.log(req.body);
-    await coursesSchema.findByIdAndUpdate({_id:req.params.id},req.body);
-    
+app.put(
+  "/courses/:id",
+  isLoggedIn,
+  isAuthor,
+  catchErrorAsync(async (req, res) => {
+    await coursesSchema.findByIdAndUpdate(
+      { _id: req.params.id },
+      { ...req.body, author: req.session.user_id }
+    );
+
     res.redirect(`/courses/${req.params.id}`);
-})
-app.delete("/courses/:id",async (req,res)=>{
+  })
+);
+
+app.delete(
+  "/courses/:id",
+  isLoggedIn,
+  isAuthor,
+  catchErrorAsync(async (req, res) => {
     await coursesSchema.findByIdAndDelete(req.params.id);
     res.redirect("/courses");
-})
-app.patch("/courses/:id/addMinutes", async(req,res)=>{ 
-    const course = await coursesSchema.findById(req.params.id);
-    let amount=course.usersTime;
-    if(amount+parseInt(req.body.usersTime) <= course.time){
-        course.usersTime +=parseInt(req.body.usersTime);
-        await course.save();
-    }
-    if(req.query.show){
-        res.redirect(`/courses/${req.params.id}`);
-    }else{
-        res.redirect("/courses");
-    }
-    
-})
+  })
+);
 
-app.patch("/courses/:id/archive",async(req,res)=>{
+app.patch(
+  "/courses/:id/addMinutes",
+  isLoggedIn,
+  isAuthor,
+  catchErrorAsync(async (req, res) => {
+    const course = await coursesSchema.findById(req.params.id);
+    let amount = course.usersTime;
+    if (amount + parseInt(req.body.usersTime) <= course.time) {
+      course.usersTime += parseInt(req.body.usersTime);
+      await course.save();
+    }
+    if (req.query.show) {
+      res.redirect(`/courses/${req.params.id}`);
+    } else {
+      res.redirect("/courses");
+    }
+  })
+);
+
+app.patch(
+  "/courses/:id/archive",
+  isLoggedIn,
+  isAuthor,
+  catchErrorAsync(async (req, res) => {
     const course = await coursesSchema.findById(req.params.id);
     course.isArchived = true;
     await course.save();
     res.redirect("/courses");
-})
+  })
+);
 
-app.patch("/courses/:id/reset",async (req,res)=>{
-    await coursesSchema.findByIdAndUpdate({_id:req.params.id},{usersTime:0,isArchived:false});
-    res.redirect("/courses")
-})
+app.patch(
+  "/courses/:id/reset",
+  isAuthor,
+  isLoggedIn,
+  catchErrorAsync(async (req, res) => {
+    await coursesSchema.findByIdAndUpdate(
+      { _id: req.params.id },
+      { usersTime: 0, isArchived: false }
+    );
+    res.redirect("/courses");
+  })
+);
 
-app.get("/history",async (req,res)=>{
-    const allcourses = await coursesSchema.find({isArchived:true});
+app.get(
+  "/history",
+  isLoggedIn,
+  catchErrorAsync(async (req, res) => {
+    const allcourses = await coursesSchema.find({
+      isArchived: true,
+      author: req.session.user_id,
+    });
 
-    res.render("history",{allcourses});
-})
+    res.render("history", { allcourses });
+  })
+);
 
-app.listen(PORT,()=>{
-    console.log(`Serwer nasłuchuje na localhost:${PORT}`);
-})
-
+app.listen(PORT, () => {
+  console.log(`Serwer nasłuchuje na localhost:${PORT}`);
+});
